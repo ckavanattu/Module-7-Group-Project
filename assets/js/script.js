bdb75827c19b39f7afc799bef8066d794b93de79
ckavanattu <chackojk@gmail.com>
2022-04-27 21:20:31 -0700

    added some js for 2nd api

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,9 @@ fetch (apiUrl)
 .then(function(data){
     console.dir(data)
     var movieResults= data.Search
-    console.log(movieResults)
-
+    // console.log(movieResults)
+    
+    
     for(i=0; i<movieResults.length; i++) {
         var moviePicture = movieResults[i].Poster
         console.log(moviePicture)
@@ -19,8 +20,27 @@ fetch (apiUrl)
         // moviePosterEl.classList = ("max-width: 450px")
         moviePosterList.appendChild(moviePosterEl);
         movieListEl.appendChild(moviePosterList);
-        console.log(moviePosterEl)
-        console.log(movieListEl)
+        // console.log(moviePosterEl)
+        // console.log(movieListEl)       
+        var movieName = movieResults[0].Title
     
     }
+
+    var apiUrlTwo = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + movieName + "&api-key=zeYpOZOvVZhX3uxBJckkZIyWGVeFLuEy"
+
+    fetch(apiUrlTwo)
+    .then (function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        // console.dir(data)
+        var movieInfo = data.results
+        console.dir(movieInfo)
+        for (i=0; i<movieInfo.length; i++){
+        var movieSummary= movieInfo[i].summary_short
+        console.log(movieSummary)
+        }
+
+})
+
 })
